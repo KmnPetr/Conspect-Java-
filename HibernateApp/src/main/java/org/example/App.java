6150c723461ebaec9f5aafdf033c024a5a2c0699
KmnPetr <127046890+KmnPetr@users.noreply.github.com>
2023-04-04 05:55:14 +0300

    ConspectJava

--- HibernateApp/src/main/java/org/example/App.java
+++ HibernateApp/src/main/java/org/example/App.java
@@ -17,7 +17,6 @@ public class App
 
         SessionFactory sessionFactory=configuration.buildSessionFactory();
         Session session=sessionFactory.getCurrentSession();
-
         try {
             session.beginTransaction();
             Person person=session.get(Person.class,1/*это @Id*/);
@@ -26,6 +25,57 @@ public class App
             System.out.println(person.getAge());
 
             session.getTransaction().commit();//применили транзакцию
+        }finally {
+            sessionFactory.close();
+        }
+        //////////////////////////////////////
+        try {
+            session.beginTransaction();
+
+            Person person1=new Person("Test1",30);
+            Person person2=new Person("Test2",40);
+            Person person3=new Person("Test3",50);
+//            session.save(person1);//устарела
+            session.persist(person1);
+            session.persist(person2);
+            session.persist(person3);
+
+            session.getTransaction().commit();//применили транзакцию
+        }finally {
+            sessionFactory.close();
+        }
+        /////////////////////////////////
+        try {
+            session.beginTransaction();
+
+            Person person=session.get(Person.class,2);//сначала получим чела по id
+            person.setName("NewName");
+            //hibernate сам вызовет update
+            session.getTransaction().commit();//применили транзакцию
+        }finally {
+            sessionFactory.close();
+        }
+        //////////////////////////////
+        try {
+            session.beginTransaction();
+            Person person=session.get(Person.class,1);
+//            session.delete(person);//устарела
+            session.remove(person);//вызовет delete
+
+            session.getTransaction().commit();
+        }finally {
+            sessionFactory.close();
+        }
+        ////////////////////////////////////
+        try {
+            session.beginTransaction();
+
+            Person person=new Person("SomePerson",60);
+            session.persist(person);
+
+            session.getTransaction().commit();
+
+            System.out.println(person.getId());
         }finally {
             sessionFactory.close();
         }
