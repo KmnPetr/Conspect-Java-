74569d21db4794f3be4dbe47d323095327652335
KmnPetr <127046890+KmnPetr@users.noreply.github.com>
2023-04-22 05:39:14 +0300

    ConspectJava

--- FirstSecurityApp/src/main/java/com/example/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/com/example/FirstSecurityApp/config/SecurityConfig.java
@@ -4,13 +4,14 @@ import com.example.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration/*WebSecurityConfigurerAdapter это по уроку так*/{
+public class SecurityConfig extends /*WebSecurityConfiguration*/WebSecurityConfigurerAdapter /*это по уроку так*/{
     //нужно при использовании AuthProvider
     /*private final AuthProviderImpl authProvider;
     @Autowired
@@ -21,6 +22,22 @@ public class SecurityConfig extends WebSecurityConfiguration/*WebSecurityConfigu
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        //конфигурируем сам спринг секьюрити
+        //конфигурируем авторизацию
+        http.authorizeHttpRequests/*неуверен*/()//настройка авторизации
+                .requestMatchers("/auth/login","/error").permitAll()//на эти 2 странички пускаем всех пользователей
+                .anyRequest().authenticated()//на все остальные запросы он должен быть аутентифицирован
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")//другую страничку для логина
+                .loginProcessingUrl("/process_login")//сюда прийдут данные с формы можно написать любой другой адрес,на форме он тоже обозначен
+                .defaultSuccessUrl("/hello",true)//url после успешной аутентификации
+                .failureUrl("/auth/login?error");//в случае неуспешной аутентификации url с параметром ошибки
+
     }
 
     //настраиваем аутентификацию
